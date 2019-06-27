d8ccaf4951ee2104aa263c012392244d656b0fb5
Noah <n-k@live.ca>
2019-06-27 13:53:43 +0200

    Simple to do list

---
+++ app.js
@@ -0,0 +1,90 @@
+(function () {
+
+    let tasksUl = document.querySelector('ul#tasks');
+    let taskInput = document.querySelector('input#task');
+    let form = document.querySelector('form#taskForm');
+    let tasksArray = [];
+    let clearButton = document.querySelector('button#clear');
+
+    populateTasksArray();
+    getTasks();
+    addInputDblClickEvents();
+    addRemoveTaskClickEvents();
+
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
+
+        if (taskInput.value !== "") tasksArray.push(taskInput.value);
+        taskInput.value = "";
+        localStorage.setItem('tasks', JSON.stringify(tasksArray));
+        getTasks();
+        addInputDblClickEvents();
+        addRemoveTaskClickEvents();
+    });
+
+    function getTasks() {
+        if (tasksArray.length > 0) {
+            tasksUl.innerHTML = "";
+            tasksArray.forEach((task, index) => {
+                let li = document.createElement('li');
+                li.innerHTML = `<input type="text" value="${task}" readonly /> <span class="removeTask btn btn-small btn-primary float-right">remove task</span>`;
+                li.classList.add('list-group-item');
+                li.setAttribute('data-id', index);
+                tasksUl.appendChild(li);
+            });
+        } else {
+            tasksUl.textContent = 'There are no tasks at the moment.';
+        }
+    }
+
+    function populateTasksArray() {
+        if (localStorage.getItem("tasks") !== null) {
+            let tasks = JSON.parse(localStorage.getItem('tasks'));
+            tasks.forEach(task => {
+                tasksArray.push(task);
+            });
+        }
+    }
+
+    function addInputDblClickEvents() {
+        let inputs = document.querySelectorAll('ul#tasks input[type="text"]');
+
+        inputs.forEach(input => {
+            input.addEventListener('dblclick', function (e) {
+                e.target.removeAttribute('readonly');
+            });
+            input.addEventListener('blur', function (e) {
+                e.target.setAttribute('readonly', true);
+            });
+            input.addEventListener('change', function (e) {
+                let index = e.target.parentElement.dataset.id;
+                tasksArray[index] = e.target.value;
+                localStorage.setItem('tasks', JSON.stringify(tasksArray));
+            });
+        });
+    }
+
+    function addRemoveTaskClickEvents() {
+        let spans = document.querySelectorAll('ul#tasks span.removeTask');
+
+        spans.forEach(span => {
+            span.addEventListener('click', function (e) {
+                if (!confirm('Confirm deletion')) return;
+                let removeSpan = e.target;
+                let index = e.target.parentElement.dataset.id;
+                tasksArray.splice(index, 1);
+                localStorage.setItem('tasks', JSON.stringify(tasksArray));
+                localStorage.removeItem(removeSpan.parentElement);
+                removeSpan.parentElement.remove();
+            });
+        });
+    }
+
+    clearButton.addEventListener('click', function () {
+        if (!confirm('Confirm deleting all items')) return;
+        localStorage.clear();
+        location.reload();
+        tasksUl.textContent = 'There are no tasks at the moment.';
+    });
+
+})();
